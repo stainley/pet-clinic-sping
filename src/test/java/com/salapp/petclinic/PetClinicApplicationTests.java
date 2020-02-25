e5c6e54a37c095ceeaca988228071f2ba839ff3e
Stainley Lebron <saiyamans@gmail.com>
2020-02-24 22:42:56 -0400

    Added Reporting plugins

--- src/test/java/com/salapp/petclinic/PetClinicApplicationTests.java
+++ src/test/java/com/salapp/petclinic/PetClinicApplicationTests.java
@@ -1,5 +1,6 @@
 package com.salapp.petclinic;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class PetClinicApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(5, 5);
 	}
 
 }
