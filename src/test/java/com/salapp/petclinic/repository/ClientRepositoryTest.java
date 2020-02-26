d9bf68e8645ff82d2eb407474ad690c536427aea
Stainley Lebron <saiyamans@gmail.com>
2020-02-25 21:34:35 -0400

    Client model

--- src/test/java/com/salapp/petclinic/repository/ClientRepositoryTest.java
+++ src/test/java/com/salapp/petclinic/repository/ClientRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.salapp.petclinic.repository;
 
 import com.salapp.petclinic.model.Client;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Stainley Lebron
  * @since 2/25/20.
  */
+
 
 @DataJpaTest
 public class ClientRepositoryTest {
@@ -21,6 +25,7 @@ public class ClientRepositoryTest {
     @Autowired
     ClientRepository clientRepository;
 
+    @Test
     public void whenFindByName_ThenReturnClient() {
         // Given
         Client client = new Client("Stainley", "Active");
