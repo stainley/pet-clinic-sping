40a93a61ee5aba17766419d3e490c53186859f29
Stainley Lebron <saiyamans@gmail.com>
2020-03-02 20:56:18 -0400

    Fixed test

--- src/test/java/com/salapp/petclinic/repository/ClientRepositoryTest.java
+++ src/test/java/com/salapp/petclinic/repository/ClientRepositoryTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
